d467a3e328d766b24d31563a3ae7d4149f7b6a6f
Alessio <alss.ldt@gmail.comm>
2025-11-21 14:52:16 +0100

    adjusted imports for shell layout and language switcher components

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,3 @@
-import React from 'react';
 import { Routes, Route, Navigate } from 'react-router-dom';
 import { ShellLayout } from './components/ShellLayout';
 import { HomePage } from './pages/HomePage';
